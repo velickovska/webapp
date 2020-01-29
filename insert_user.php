c5bece78e31f5a5b826720d03c2925a88da19182
Maya <37115588+Maya084@users.noreply.github.com>
2020-01-29 22:17:37 +0100

    signup working version

--- insert_user.php
+++ insert_user.php
@@ -1,34 +1,42 @@
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+</head>
+
+</html>
+
 <?php
 include("includes/connection.php");
 
-if(isset($POST["sign_up"]))
+if(isset($_POST["sign_up"]))  
 {
-    $ime = htmlentities(mysqli_real_escape_string($conn,$POST["ime"]));
-    $prezime = htmlentities(mysqli_real_escape_string($conn,$POST["prezime"])) ;
-    $username = htmlentities(mysqli_real_escape_string($conn,$POST["username"]));
-    $email = htmlentities(mysqli_real_escape_string($conn,$POST["email"]));
-    $tel_br = htmlentities(mysqli_real_escape_string($conn,$POST["phone"]));
-    $drzava = htmlentities(mysqli_real_escape_string($conn,$POST["country"]));
-    $grad = htmlentities(mysqli_real_escape_string($conn,$POST["state"]));
+    $ime =$_POST["ime"];
+    $prezime =$_POST["prezime"] ;
+    $username = $_POST["username"];
+    $email = $_POST["email"]  ;
+    $tel_br = $_POST["phone"]  ;
+    $drzava = $_POST["country"]  ;
+    $grad =  $_POST["state"]  ;
     $posts = "no";
-    $password = htmlentities(mysqli_real_escape_string($conn,$POST["password"]));
-    $opis = htmlentities(mysqli_real_escape_string($conn,$POST["opis"]));
+    $password = $_POST["pass"]  ;
+    $opis = $_POST["opis"]  ;
     
 
     if(strlen($password) < 8)
     {
-        echo"<script> alert('Лозинката мора да биде долга најмалку 8 знаци')</script>";
+        echo"<script charset=\"utf-8\"> alert('Лозинката мора да биде долга најмалку 8 знаци'); history.back();</script>";
         exit();
     }
 
     $check_email = "select * from user where email='$email'";
-    $run_email = mysqli_query($check_email);
+    $run_email = mysqli_query($conn, $check_email);
     $check = mysqli_num_rows($run_email);
     if($check == 1)
     {
-        echo"<script> alert('Е-поштата веќе постои, Ве молиме обидете се повторно')</script>";
-        echo"<script> window.open('signup.php','_self')</script>";
-        exit();
+        echo"<script charset=\"utf-8\"> alert('Е-поштата веќе постои, Ве молиме обидете се повторно'); history.back();</script>";
+        
     }
     $rand = rand(1, 5);
     $profilna = "images/user" . $rand . ".png";
@@ -38,20 +46,20 @@ if(isset($POST["sign_up"]))
 
     $token = hash('ripemd128',"$salt1$password$salt2",FALSE);
 
-    $insert = "insert into user (ime, prezime, username, password, telbr, grad, drzava, profile_pic, opis, posts) 
-    values ('$ime', '$prezime', '$username','$token','$tel_br','$grad','$drzava','$profilna','$opis','$posts')";
+    $insert = "insert into user (ime, prezime, username, password, email, telbr, grad, drzava, profile_pic, opis, posts) 
+    values ('$ime', '$prezime', '$username','$token', '$email','$tel_br','$grad','$drzava','$profilna','$opis','$posts')";
     $query = mysqli_query($conn, $insert);
 
     if($query)
     {
-        echo"<script> alert('Успешна регистрација!')</script>";
-        echo"<script> window.open('signin.php','_self')</script>";
+        echo"<script charset=\"utf-8\"> alert('Успешна регистрација!')</script>";
+        echo"<script charset=\"utf-8\"> window.open('signin.html','_self')</script>";
         
     }
     else
     {
-        echo"<script> alert('Неуспешна регистрација.\n Ве молиме обидете се повторно.')</script>";
-        echo"<script> window.open('signup.php','_self')</script>";
+        echo"<script charset=\"utf-8\"> alert('Неуспешна регистрација.\n Ве молиме обидете се повторно.'); history.back();</script>";
+        
         
     }
 
