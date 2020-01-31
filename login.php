f2af8def81aa71568c28b76b088980317290d187
Maya <37115588+Maya084@users.noreply.github.com>
2020-01-31 17:12:17 +0100

    rabotna login verzija
    dokolku ne postoe e-mailo/username app zaglavuva i ostanuva na prazna login.php. Dokolku postoe e-mailo/username rabote sovrseno

--- login.php
+++ login.php
@@ -13,32 +13,55 @@
 include("includes/connection.php");
 if(isset($_POST['login']))
 {
-if(isset($_POST['username']) && isset($_POST['password']))
-{
+
   $user = $_POST['username'];
   $pass = $_POST['password'];
-  $query = "SELECT * FROM user WHERE username='$username'";
-  $result = mysql_query($query);
-  if(!$result) die ('You are not registered!');
-  else {
-  $row = mysql_fetch_row($result);
-  $salt1 = "ap3gmh*!";
-  $salt2 = "o_7p@&";
-  $token = hash('ripemd128',"$salt1$pass$salt2");
 
-  if($token == $row[4])
-  {
-    echo "Hi $row[1], you are now logged in as $row[1] $row[2]";
-    session_start();
-    $_SESSION['username'] = $row[3];
-    $_SESSION['surname'] = $row[2];
-    $_SESSION['firstname'] = $row[1];
+  $check_email = "select * from user where email='$user'";
+    $run_email = mysqli_query($conn, $check_email);
+    $check = mysqli_num_rows($run_email);
 
-    echo "<p><a href=main.php>Click here to continue</a></p>";
+  $check_user = "select * from user where username='$user'";
+    $run_user = mysqli_query($conn, $check_user);
+    $check2 = mysqli_num_rows($run_user);  
+
+  if($check2 == 0 &&  $check==0)
+    {
+      echo"<script charset=\"utf-8\"> alert('Не постои корисничкото име или е-пошта. 
+      \nВе молиме обидете се повторно.'); history.back();</script>";
+
+        
+    }
+  
+    else
+    {
+      
+      $salt1 = "ap3gmh*!";
+      $salt2 = "o_7p@&";
+      $token = hash('ripemd128',"$salt1$pass$salt2");
+        
+
+        if($check2==1)
+        $result = $run_user;
+        else
+        $result = $run_email;
+
+        $row = mysqli_fetch_assoc($result); 
+        if(($row["password"]) == $token)
+        {
+          session_start();
+          $_SESSION['user'] = $row["username"];
+          $_SESSION['ime'] = $row["ime"];
+          $_SESSION['prezime'] = $row["prezime"];
+          echo"<script charset=\"utf-8\"> alert('Успешна најава'); </script>";
+          header("Location: main.php");
+        }
+        else
+        {
+          echo"<script charset=\"utf-8\"> alert('Погрешна лозинка'); history.back();</script>";
+        }
+    }  
   }
-  else
-  die("Invalid username/password combination");
-}
-}
-}
+
+  
 ?>
